605cbc55538271862a1f3afe92ad6467ba924b1e
vinniipooh <vinniipuh@gamil.com>
2017-04-29 18:10:31 +0300

    Initial commit

---
+++ src/vk/controllers/enterIDController.java
@@ -0,0 +1,7 @@
+package vk.controllers;
+
+/**
+ * Created by Александр on 29.04.2017.
+ */
+public class enterIDController {
+}
---
+++ src/vk/interfaces/SeachDataInterface.java
@@ -0,0 +1,15 @@
+package vk.interfaces;
+
+/**
+ * Created by Александр on 29.04.2017.
+ */
+public interface SeachDataInterface {
+    void checkId(String id);
+
+    void searchforData(String id);
+
+    void getFullName();
+
+    void getStatus();
+
+}
